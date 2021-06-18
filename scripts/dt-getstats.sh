a1074dbec1a4010ffabc399ef7e07fa3b57a7b34
Rafael Trevisan <rafa.trevi@hotmail.com>
2021-06-17 22:31:40 -0300

    stats talvez

--- scripts/dt-getstats.sh
+++ scripts/dt-getstats.sh
@@ -1,4 +1,17 @@
+for c in $(seq 1 5)
+do
+   date +"%T.%N" >> "stats.log"
+   docker stats --no-stream >> "stats.log"
+   sleep 5
+done
+
 for i in "$*"
 do
-   echo $i
+   docker restart $i
+done
+
+while :; do
+   date +"%T.%N" >> "stats.log"
+   docker stats --no-stream >> "stats.log"
+   sleep 5
 done
