90c050ddfd2fe50f7be9c2a1afd744ecf5d42894
Rafael Trevisan <rafa.trevi@hotmail.com>
2021-08-25 16:57:53 -0300

    new tests

--- scripts/dt-getstats.sh
+++ scripts/dt-getstats.sh
@@ -3,7 +3,7 @@ for REC in $(seq 0 20)
 do
 date +"%T.%N" >> "stats.log"
 echo "saving n $REC at"; date +"%T.%N"
-docker stats --no-stream >> "stats.log"
+docker stats --no-stream >> "stats.log" & 
 let REC=REC+1
 sleep 5
 done
