5e367e18856c68d59121657b35d94fe16077d10b
Rafael Trevisan <rafa.trevi@hotmail.com>
2021-10-26 21:23:55 -0300

    changed stat script

--- scripts/dt-getstats.sh
+++ scripts/dt-getstats.sh
@@ -1,9 +1,8 @@
 REC=0
-for REC in $(seq 0 10) 
-do
+while :; do
 date +"%T.%N" >> "stats.log"
 echo "saving n $REC at"; date +"%T.%N"
 docker stats --no-stream >> "stats.log" & 
 let REC=REC+1
-sleep 4
+sleep 5
 done
