5f4d10bfc866636faee96f5be64299b821e0af20
Rafael Trevisan <rafa.trevi@hotmail.com>
2022-01-27 06:03:02 -0300

    changes to repo

--- Article_test/experiment_start.sh
+++ Article_test/experiment_start.sh
@@ -2,9 +2,9 @@ docker service scale test_mqtt-cluster=1 test_etcd=1
 sleep 5
 docker service scale test_mqtt-manager=1 test_twin-A=1 test_twin-B=1
 sleep 5
-docker service scale test_client-A=1
+docker service scale test_clients-A=1
 for i in 100; do
 echo sleplt i * 5
 sleep 5
 done
-docker service scale test_client-B=1
+docker service scale test_clients-B=1
