37eae306c8d2586924785cfc210bd7d2f98d7c77
Rafael Trevisan <rafa.trevi@hotmail.com>
2022-01-31 03:09:13 -0300

    changed experiment script

--- Article_test/experiment_start.sh
+++ Article_test/experiment_start.sh
@@ -8,3 +8,33 @@ echo sleplt i * 5
 sleep 5
 done
 docker service scale test_clients-B=1
+for i in 100; do
+echo sleplt i * 5
+sleep 5
+done
+docker service scale test_clients-C=1
+for i in 100; do
+echo sleplt i * 5
+sleep 5
+done
+docker service scale test_clients-D=1
+for i in 100; do
+echo sleplt i * 5
+sleep 5
+done
+docker service scale test_clients-E=1
+for i in 100; do
+echo sleplt i * 5
+sleep 5
+done
+docker service scale test_clients-F=1
+for i in 100; do
+echo sleplt i * 5
+sleep 5
+done
+docker service scale test_clients-G=1
+for i in 100; do
+echo sleplt i * 5
+sleep 5
+done
+docker service scale test_clients-H=1
