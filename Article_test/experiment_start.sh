1585e00943e05fd987ee3932ad4ea73230e35b1e
Rafael Trevisan <rafa.trevi@hotmail.com>
2022-01-31 03:32:37 -0300

    changes to  script

--- Article_test/experiment_start.sh
+++ Article_test/experiment_start.sh
@@ -3,38 +3,59 @@ sleep 5
 docker service scale test_mqtt-manager=1 test_twin-A=1 test_twin-B=1 test_twin-D=1 test_twin-C=1 test_twin-E=1 test_twin-F=1 test_twin-G=1 test_twin-H=1
 sleep 5
 docker service scale test_clients-A=1
-for i in 100; do
-echo sleplt i * 5
-sleep 5
+i=0
+while [ $i -ne 500 ]
+do
+        i=$(($i+1))
+        echo "$i, Client A"
+        sleep 1
 done
 docker service scale test_clients-B=1
-for i in 100; do
-echo sleplt i * 5
-sleep 5
+i=0
+while [ $i -ne 500 ]
+do
+        i=$(($i+1))
+        echo "$i, Client A, B"
+        sleep 1
 done
 docker service scale test_clients-C=1
-for i in 100; do
-echo sleplt i * 5
-sleep 5
+i=0
+while [ $i -ne 500 ]
+do
+        i=$(($i+1))
+        echo "$i, Client A B C"
+        sleep 1
 done
 docker service scale test_clients-D=1
-for i in 100; do
-echo sleplt i * 5
-sleep 5
+i=0
+while [ $i -ne 500 ]
+do
+        i=$(($i+1))
+        echo "$i, Client A B C D"
+        sleep 1
 done
 docker service scale test_clients-E=1
-for i in 100; do
-echo sleplt i * 5
-sleep 5
+i=0
+while [ $i -ne 500 ]
+do
+        i=$(($i+1))
+        echo "$i, Client A B C D E"
+        sleep 1
 done
 docker service scale test_clients-F=1
-for i in 100; do
-echo sleplt i * 5
-sleep 5
+i=0
+while [ $i -ne 500 ]
+do
+        i=$(($i+1))
+        echo "$i, Client A B C D E F"
+        sleep 1
 done
 docker service scale test_clients-G=1
-for i in 100; do
-echo sleplt i * 5
-sleep 5
+i=0
+while [ $i -ne 500 ]
+do
+        i=$(($i+1))
+        echo "$i, Client A B C D E F G"
+        sleep 1
 done
 docker service scale test_clients-H=1
