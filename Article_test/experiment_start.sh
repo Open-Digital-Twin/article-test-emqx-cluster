9983c87dabbfda6fcbad262a6624a3d95b272d99
Rafael Trevisan <rafa.trevi@hotmail.com>
2022-02-16 16:35:08 -0300

    test diverse twins

--- Article_test/experiment_start.sh
+++ Article_test/experiment_start.sh
@@ -2,9 +2,9 @@ docker service scale test_mqtt-cluster=1 test_etcd=1
 sleep 5
 docker service scale test_mqtt-manager=1 test_twin-A=1 test_twin-B=1 test_twin-D=1 test_twin-C=1 test_twin-E=1 test_twin-F=1
 sleep 5
-docker service scale test_clients-A=1
+docker service scale test_clients-A=1 test_clients-G=1
 i=0
-while [ $i -ne 500 ]
+while [ $i -ne 1000 ]
 do
         i=$(($i+1))
         echo "$i, Client A"
@@ -12,7 +12,7 @@ do
 done
 docker service scale test_clients-B=1
 i=0
-while [ $i -ne 500 ]
+while [ $i -ne 1000 ]
 do
         i=$(($i+1))
         echo "$i, Client A, B"
@@ -20,7 +20,7 @@ do
 done
 docker service scale test_clients-C=1
 i=0
-while [ $i -ne 500 ]
+while [ $i -ne 1000 ]
 do
         i=$(($i+1))
         echo "$i, Client A B C"
@@ -28,7 +28,7 @@ do
 done
 docker service scale test_clients-D=1
 i=0
-while [ $i -ne 500 ]
+while [ $i -ne 1000 ]
 do
         i=$(($i+1))
         echo "$i, Client A B C D"
