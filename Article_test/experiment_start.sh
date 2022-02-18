39d5c2afa08da634816a9f12c64ddeea4ccfc3bf
Rafael Trevisan <rafa.trevi@hotmail.com>
2022-02-18 14:00:44 -0300

    smaller tests

--- Article_test/experiment_start.sh
+++ Article_test/experiment_start.sh
@@ -2,44 +2,4 @@ docker service scale test_mqtt-cluster=1 test_etcd=1
 sleep 5
 docker service scale test_mqtt-manager=1 test_twin-A=1 test_twin-B=1 test_twin-D=1 test_twin-C=1 test_twin-E=1 test_twin-F=1
 sleep 5
-docker service scale test_clients-A=1 test_clients-G=1
-i=0
-while [ $i -ne 1000 ]
-do
-        i=$(($i+1))
-        echo "$i, Client A"
-        sleep 1
-done
-docker service scale test_clients-B=1
-i=0
-while [ $i -ne 1000 ]
-do
-        i=$(($i+1))
-        echo "$i, Client A, B"
-        sleep 1
-done
-docker service scale test_clients-C=1
-i=0
-while [ $i -ne 1000 ]
-do
-        i=$(($i+1))
-        echo "$i, Client A B C"
-        sleep 1
-done
-docker service scale test_clients-D=1
-i=0
-while [ $i -ne 1000 ]
-do
-        i=$(($i+1))
-        echo "$i, Client A B C D"
-        sleep 1
-done
-docker service scale test_clients-E=1
-i=0
-while [ $i -ne 500 ]
-do
-        i=$(($i+1))
-        echo "$i, Client A B C D E"
-        sleep 1
-done
-docker service scale test_clients-F=1
+docker service scale test_clients-A=1 test_clients-G=1 test_clients-B=1 test_clients-C=1 test_clients-D=1 test_clients-E=1 test_clients-F=1
